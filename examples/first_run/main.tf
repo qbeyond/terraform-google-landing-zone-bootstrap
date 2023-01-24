e2c6be4a649335c1736f99a483b679e939ffc84b
Thimo Limpert <ThimoLuca.Limpert@qsc.de>
2023-01-24 15:29:04 +0100

    Add example

--- examples/first_run/main.tf
+++ examples/first_run/main.tf
@@ -1,3 +1,18 @@
+resource "random_id" "prefix" {
+  byte_length = 8
+}
+
+data "google_organization" "this" {
+  domain = var.domain
+}
+
 module "bootstrap" {
-  source = "../.."
+  source         = "../.."
+  bootstrap_user = var.bootstrap_user
+  organization   = data.google_organization.this
+  prefix         = random_id.prefix.hex
+  billing_account = {
+    id              = var.billing_account_id
+    organization_id = var.billing_account_organization_id
+  }
 }
---
+++ examples/first_run/variables.tf
@@ -0,0 +1,18 @@
+variable "billing_account_id" {
+  description = "Billing account id."
+  type        = string
+}
+variable "billing_account_organization_id" {
+  description = "organization id of billing account."
+  type        = number
+}
+
+variable "domain" {
+  description = "Organization domain."
+  type        = string
+}
+
+variable "bootstrap_user" {
+  description = "User running this example. This account will be assigned to some permissions. This is only supposed to be run one time!"
+  type = string
+}
